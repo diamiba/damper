d3bdcee7ea78cadaea169e067590a89d77fe1e39
josias98 <diamitanijosiasmansour@gmail.com>
2019-10-04 00:39:33 +0100

    No asset message added

--- src/app/assets/assets/assets.component.ts
+++ src/app/assets/assets/assets.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { ModalService } from 'src/app/shared/services/modal.service';
 import { DataService } from 'src/app/shared/services/data.service';
@@ -10,20 +10,24 @@ import { ExcelService } from 'src/app/shared/services/excel.service';
     templateUrl: './assets.component.html',
     styleUrls: ['./assets.component.scss']
 })
-export class AssetsComponent implements OnInit {
+export class AssetsComponent implements OnInit,OnDestroy {
 
     constructor(private modalService:ModalService, public dataService:DataService, private excelService:ExcelService) { }
 
-    list: UserAsset[];
+    noAsset:boolean;
+    assetsSubscription:any;
     isLoading = true;
+
     ngOnInit() {
         this.isLoading = false;
-        // this.dataService.assets$.subscribe((assetsList:UserAsset[])=>{
-        //     // console.log(assetsList);
-        //     this.list = assetsList;
-        //     this.isLoading = false;
-        // })
+        this.assetsSubscription = this.dataService.assets$.subscribe((assetsList:UserAsset[])=>{
+            this.noAsset = assetsList.length===0;
+        })
         // console.log(this.dataService.assets$);
+    }
+
+    ngOnDestroy(){
+        this.assetsSubscription.unsubscribe();
     }
 
     newAsset(){
